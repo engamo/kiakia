2d52a29b43ca1c26bce583dc5a93d450936048ef
Mustapha Anthonio <engamo123@gmail.com>
2024-01-23 11:36:58 +0100

    update Navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          KiaKia
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
